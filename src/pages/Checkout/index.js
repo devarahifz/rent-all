4a4ba9554bfe8a92248ee1798f09a37021747dd7
devarahifz <devara.hifzhurrahman22@gmail.com>
2021-12-06 19:38:00 +0700

    Create Checkout Page

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Col, Container, Row, Form,Button } from 'react-bootstrap'
-import './checkout.css'
 import user from '../../assets/user.jpg'
 
 const Checkout = () => {
